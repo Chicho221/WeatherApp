1932ee29f459bb7ceed121bcc377e8371e6d833c
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-24 02:26:08 +0100

    Add multiple search option

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from weather import get_weather, city_name_check
 
 def main():
     while True:
-        city = input("Enter city: ").lower()
+        city = input("\nEnter city name (or 'exit'): ").lower()
+        if city == "exit":
+            break
         if not city_name_check(city): # Check if name is valid (not a number)
             print("Invalid city name.")
             continue
+        weather = get_weather(city)
+
+        if weather:
+            print(f"\n---- Weather Report ----")
+            print(f"City: {weather['city']}")
+            print(f"Temperature: {round(weather['temperature'], 1)}°C")
+            print(f"Weather: {weather['description'].title()}")
+            continue
         break
-
-    weather = get_weather(city)
-
-    if weather:
-        print(f"\n---- Weather Report ----")
-        print(f"City: {weather['city']}")
-        print(f"Temperature: {round(weather['temperature'], 1)}°C")
-        print(f"Weather: {weather['description'].title()}")
 
 if __name__ == "__main__":
     main()
