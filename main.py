4d7fea1c80f617847c22302cc4d7df5a8b049bd5
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-24 02:15:53 +0100

    Update output visuals

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from weather import get_weather, city_name_check
 
 def main():
     while True:
-        city = input("Enter city: ")
+        city = input("Enter city: ").lower()
         if not city_name_check(city): # Check if name is valid (not a number)
             print("Invalid city name.")
             continue
         break
-    
+
     weather = get_weather(city)
 
     if weather:
-        print(f"\nCity: {weather['city']}")
-        print(f"Temperature: {weather['temperature']}°C")
-        print(f"Weather: {weather['description']}")
+        print(f"\n---- Weather Report ----")
+        print(f"City: {weather['city']}")
+        print(f"Temperature: {round(weather['temperature'], 1)}°C")
+        print(f"Weather: {weather['description'].title()}")
 
 if __name__ == "__main__":
     main()
